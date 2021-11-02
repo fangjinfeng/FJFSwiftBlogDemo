8f2baa5f78b729070c84dd86442662aeaf640b4b
方金峰 <116418179@qq.com>
2021-11-02 18:41:53 +0800

    Initial Commit

---
+++ FJFSwiftBlogDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FJFSwiftBlogDemo
+//
+//  Created by peakfang on 2021/11/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
